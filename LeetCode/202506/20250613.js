0a9064df4b37ec70cadbcc6a8870147287ecf480
kohei240155 <k.pino519@gmail.com>
2025-06-13 23:56:12 -0700

    20250613の実績を追加

---
+++ LeetCode/202506/20250613.js
@@ -0,0 +1,28 @@
+/*
+================================================================================
+問題: 20. Valid Parentheses
+出典: https://leetcode.com/problems/valid-parentheses/description/
+================================================================================
+*/
+
+// ✅ 自分の解答
+var isValid = function(s) {
+    const stack = [];
+    const map = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    }
+
+    for (let char of s) {
+        if (char === '(' || char === '[' || char === '{') {
+            stack.push(char);
+        } else {
+            if (stack.pop() !== map[char]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+}
