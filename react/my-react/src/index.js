f935d0fd0a6158443e6fb660a2b368995b9dbd64
kohei240155 <k.pino519@gmail.com>
2025-09-05 19:56:45 -0700

    s

--- react/my-react/src/index.js
+++ react/my-react/src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import StateForm from './chap04/StateForm';
+import StateFormUC from './chap04/StateFormUC';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <StateForm />
+  <StateFormUC />
 );
