9d9a3da8e1dc181f502059e36eeb4e3afee61f37
Kohei Fujiwara <k.pino519@gmail.com>
2025-09-17 22:57:10 -0700

    s

--- react/my-react/src/index.js
+++ react/my-react/src/index.js
@@ -6,6 +6,7 @@ import StateFormUC from './chap04/StateFormUC';
 import StateForm from './chap04/StateForm';
 import FormTextArea from './chap04/FormTextarea';
 import FormSelect from './chap04/FormSelect';
+import FormList from './chap04/FormList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,5 +15,6 @@ root.render(
     <StateFormUC />
     <FormTextArea />
     <FormSelect />
+    <FormList />
   </>
 );
